17de6cde11cb74e43667583064d90de62b40b547
Tamerlan9129 <tamerlankha@code.edu.az>
2022-10-18 18:49:20 +0400

    crud operation

---
+++ front-to-back/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace front_to_back.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+              CategoryComponents = new List<CategoryComponent>();
+        }
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Ad mutleq doldurulmalidir"),MinLength(3,ErrorMessage ="Adin uzunlugu minimum 3 olmalidir")]
+        public string Title { get; set; }
+        public ICollection<CategoryComponent> CategoryComponents { get; set; }
+    }
+}
---
+++ front-to-back/Models/RecentWorkComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace front_to_back.Models
+{
+    public class RecentWorkComponent
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ad mutleq doldurulmalidir"), MinLength(3, ErrorMessage = "Adin uzunlugu minimum 3 olmalidir")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Text mutleq doldurulmalidir"), MinLength(10, ErrorMessage = "Text uzunlugu minimum 10 olmalidir")]
+        public string Text { get; set; }
+
+        [Required(ErrorMessage = "Mutleq doldurulmalidir")]
+        public string FilePath { get; set; }
+    }
+}
